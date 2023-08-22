988e948b31917534d30bd01a53c2ca972f44dfd5
Marcos Rigel <marcosrigel99@gmail.com>
2023-08-21 23:19:40 -0300

    Projeto finalizado

--- projeto-simulando-uma-conta-bancaria/src/application/Program.java
+++ projeto-simulando-uma-conta-bancaria/src/application/Program.java
@@ -1,14 +1,36 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ContaTerminal;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
 		//TODO: Conhecer e importar a classe Scanner
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
 		//TODO: Exibir as mensagens para o nosso usuario
 		
+		System.out.print("Por favor, digite o número da Agência: ");
+		String agencia = sc.nextLine();
+		System.out.print("digite o seu nome: ");
+		String nomeCliente = sc.nextLine();
+		System.out.print("digite o seu numero: ");
+		int numero = sc.nextInt();
+		System.out.print("digite o seu saldo: ");
+		double saldo = sc.nextDouble();
+		
 		//TODO: Obter pela scanner os valores digitados no terminal
 		
+		ContaTerminal conta = new ContaTerminal(numero, agencia, nomeCliente, saldo);
+		
 		//TODO: Exibir a mensagem conta criada
+		System.out.println(conta);
+		
+		sc.close();
 	}
 }
---
+++ projeto-simulando-uma-conta-bancaria/src/entities/ContaTerminal.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class ContaTerminal {
+	
+	int numero;
+	String agencia;
+	String nomeCliente;
+	double saldo;
+
+	public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.nomeCliente = nomeCliente;
+		this.saldo = saldo;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	@Override
+	public String toString() {
+		return"Olá " + getNomeCliente() 
+		+ ", obrigado por criar uma conta em nosso banco, sua agência é " + getAgencia()
+		+ ", conta " + getNumero() 
+		+ " e seu saldo " + getSaldo() + " já está disponível para saque";
+	}
+	
+	
+
+}
